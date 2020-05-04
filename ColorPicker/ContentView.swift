bc0a37177fdac917dd409738d688ec8a03e05fee
asimonv <aasimon@uc.cl>
2020-05-04 14:35:15 -0400

    fixes layout issues created because  new action view was added

--- ColorPicker/ContentView.swift
+++ ColorPicker/ContentView.swift
@@ -21,7 +21,8 @@ struct ContentView: View {
     @State var centerPosition: UnitPoint = .center
     @State var rotationValue: Angle = .zero
     @State var radialScale: CGFloat = 1.0
-    
+    @State var showImagePicker: Bool = false
+
     @State var notificationText: String = "Background saved."
     
     @State var pickedColors: [UIColor] = []
@@ -39,6 +40,10 @@ struct ContentView: View {
         self.gradientType = 0
     }
     
+    func pickImageAction() -> Void {
+        self.showImagePicker.toggle()
+    }
+    
     /*
         0: Single
         1: Linear
@@ -46,61 +51,74 @@ struct ContentView: View {
         3: Radial
      */
     func saveBackground() -> Void {
-        var image: UIImage!
-        switch gradientType {
-        case 0:
-            image = UIImage(color: UIColor.blend(colors: pickedColors), size: UIScreen.main.bounds.size)!
-        case 1:
-            image = LinearGradientView(pickedColors: $pickedColors).asImage()
-        case 2:
-            image = AngularGradientView(pickedColors: $pickedColors, rotationValue: $rotationValue, centerPosition: $centerPosition).asImage()
-        case 3:
-            image = RadialGradientView(pickedColors: $pickedColors, scale: $radialScale).asImage()
-        default:
-            image = UIImage(color: currentColor, size: UIScreen.main.bounds.size)!
-        }
-                
-        CustomPhotoAlbum.shared.save(image, completion: { result, error in
-            if error != nil {
-                // handle error
-                self.showAlert = true
-                self.alertMessage = error!.localizedDescription
-                return
-            }
-            // save successful, do something (such as inform user)
-            self.backgroundSaved = !self.backgroundSaved
-        })
+        print("????????")
+//        var image: UIImage!
+//        switch gradientType {
+//        case 0:
+//            image = UIImage(color: UIColor.blend(colors: pickedColors), size: UIScreen.main.bounds.size)!
+//        case 1:
+//            image = LinearGradientView(pickedColors: $pickedColors).asImage()
+//        case 2:
+//            image = AngularGradientView(pickedColors: $pickedColors, rotationValue: $rotationValue, centerPosition: $centerPosition).asImage()
+//        case 3:
+//            image = RadialGradientView(pickedColors: $pickedColors, scale: $radialScale).asImage()
+//        default:
+//            image = UIImage(color: currentColor, size: UIScreen.main.bounds.size)!
+//        }
+//
+//        CustomPhotoAlbum.shared.save(image, completion: { result, error in
+//            if error != nil {
+//                // handle error
+//                self.showAlert = true
+//                self.alertMessage = error!.localizedDescription
+//                return
+//            }
+//            // save successful, do something (such as inform user)
+//            self.backgroundSaved = !self.backgroundSaved
+//        })
     }
     
     var body: some View {
-        ZStack(alignment: .topTrailing) {
+        ZStack {
             GradientView(gradientType: $gradientType, pickedColors: $pickedColors, rotationValue: $rotationValue, centerPosition: $centerPosition, radialScale: $radialScale)
-            // background hack to make ColorPickerView have gesture preference over GradientView
-            ColorPickerView(chosenColor: $currentColor, isDragging: $isDragging)
-                .frame(width: 50, height: 200).padding([.top], 80).background(Color.white.opacity(0.0001)).padding([.trailing], 35)
-            
-            VStack(alignment: .leading) {
-                if !isDragging {
-                    VStack(alignment: .trailing) {
-                        Spacer().frame(maxWidth: .infinity)
-                        if pickedColors.count > 0 {
-                            ColorsView(pickedColors: $pickedColors)
+            GeometryReader { geometry in
+                VStack {
+                    // background hack to make ColorPickerView have gesture preference over GradientView
+                    HStack {
+                        Spacer()
+                        VStack {
+                            ColorPickerView(chosenColor: self.$currentColor, isDragging: self.$isDragging)
+                            .frame(width: 60, height: 200)
+                            .padding([.top], 80)
+                            .background(Color.white.opacity(0.0001))
+                            .padding([.trailing], 25)
+                            
+                            if !self.isDragging {
+                                VStack(alignment: .trailing) {
+                                    if self.pickedColors.count > 0 {
+                                        ColorsView(pickedColors: self.$pickedColors)
+                                    }
+                                }
+                                .transition(.move(edge: .trailing))
+                                .animation(.ripple())
+                                .padding(EdgeInsets(top: 10, leading: 0, bottom: 30, trailing: 22))
+                            }
                         }
-                        ActionsView(pickedColors: $pickedColors, shuffleAction: shuffleAction, addColor: addColor, saveBackground: saveBackground, clearAction: clearAction)
                     }
-                    .transition(.move(edge: .trailing))
-                    .animation(.ripple())
-                    .padding(EdgeInsets(top: 0, leading: 0, bottom: 30, trailing: 30))
-                }
-                if backgroundSaved {
-                    SavedView(currentColor: $currentColor, notificationText: $notificationText, callback: {
-                        DispatchQueue.main.asyncAfter(deadline: .now() + 1.5) {
-                             withAnimation {
-                                 self.backgroundSaved = false
+                    if self.backgroundSaved {
+                        SavedView(currentColor: self.$currentColor, notificationText: self.$notificationText, callback: {
+                            DispatchQueue.main.asyncAfter(deadline: .now() + 1.5) {
+                                 withAnimation {
+                                     self.backgroundSaved = false
+                                 }
                              }
-                         }
-                     }).padding(.all)
-                 }
+                         }).padding(.all)
+                     }
+                Spacer()
+                ActionsView(shuffleAction: self.shuffleAction, addColor: self.addColor, saveBackground: self.saveBackground, clearAction: self.clearAction, pickImageAction: self.pickImageAction)
+                }
+                .frame(width: geometry.size.width, height: nil)
+                .padding([.bottom], 40)
             }
         }
         .edgesIgnoringSafeArea(.all)
@@ -109,6 +127,12 @@ struct ContentView: View {
                     UIApplication.shared.open(URL(string: UIApplication.openSettingsURLString)!)
             })
 
+        }
+        .sheet(isPresented: $showImagePicker) {
+            ImagePicker(sourceType: .photoLibrary) { image in
+                let extractedColors = image.getColors()
+                self.pickedColors.append(contentsOf: [(extractedColors?.detail)!, (extractedColors?.background)!, (extractedColors?.primary)!, (extractedColors?.secondary)!])
+            }
         }
     }
 }
--- ColorPicker/Views/ActionsView.swift
+++ ColorPicker/Views/ActionsView.swift
@@ -10,58 +10,71 @@ import SwiftUI
 
 
 struct ActionsView: View {
-    @Binding var pickedColors: [UIColor]
+    @State var open = false
     
     var shuffleAction: () -> Void
     var addColor: () -> Void
     var saveBackground: () -> Void
     var clearAction: () -> Void
+    var pickImageAction: () -> Void
 
     var body: some View {
-        HStack(alignment: .center) {
-            Button(action: clearAction) {
-                Image(systemName: "clear.fill")
-                .resizable()
-                .frame(width: 30, height: 30)
-                .foregroundColor(Color.white.opacity(0.8))
-                .shadow(radius: 5)
-                .padding(.all)
-            }
-            
-            Button(action: shuffleAction) {
-                Image(systemName: "shuffle")
-                .resizable()
-                .frame(width: 30, height: 30)
-                .foregroundColor(Color.white.opacity(0.8))
-                .shadow(radius: 5)
-                .padding(.all)
-            }.disabled(pickedColors.count <= 1)
-            
-            Button(action: addColor) {
-                Image(systemName: "plus.app.fill")
-                .resizable()
-                .frame(width: 30, height: 30)
-                .foregroundColor(Color.white.opacity(0.8))
-                .shadow(radius: 5)
-                .padding(.all)
+        ZStack {
+            MenuItem(open: $open, icon: "trash", action: clearAction, offsetY: -90, color: .pink)
+            MenuItem(open: $open, icon: "photo", action: pickImageAction, offsetX: -60, offsetY: -60, delay: 0.1, color: .blue)
+            MenuItem(open: $open, icon: "shuffle", action: shuffleAction, offsetX: -80, delay: 0.2, color: .blue)
+            MenuItem(open: $open, icon: "plus", action: addColor, offsetX: 60, offsetY: -60, delay: 0.3, color: .blue)
+            MenuItem(open: $open, icon: "arrow.down.to.line", action: saveBackground, offsetX: 80, delay: 0.4, color: Color(hex: "#00b894"))
 
+            Button(action: { self.open.toggle()}) {
+                Image(systemName: "plus.circle.fill")
+                    .resizable()
+                    .frame(width: 60, height: 60)
+                    .foregroundColor(.blue)
+                    .rotationEffect(.degrees(open ? 45 : 0))
+                    .animation(.spring(response: 0.2, dampingFraction: 0.4, blendDuration: 0))
             }
-            
-            Button(action: saveBackground) {
-                Image(systemName: "square.and.arrow.down.fill")
-                .resizable()
-                .frame(width: 30, height: 30)
-                .foregroundColor(Color.white.opacity(0.8))
-                .shadow(radius: 5)
-                .padding(.all)
-            }.disabled(pickedColors.count < 1)
-
+            .background(Color.white)
+            .clipShape(Circle())
+            .overlay(
+                Circle().stroke(Color.white, lineWidth: 3))
+            .padding([.horizontal], 5)
         }
     }
 }
 
 struct ActionsView_Previews: PreviewProvider {
     static var previews: some View {
-        ActionsView(pickedColors: Binding.constant([]), shuffleAction: {}, addColor: {}, saveBackground: {}, clearAction: {})
+        ActionsView(shuffleAction: {}, addColor: {}, saveBackground: {}, clearAction: {}, pickImageAction: {})
+    }
+}
+
+struct MenuItem: View {
+    @Binding var open: Bool
+    
+    var icon: String
+    var action: () -> Void
+    var offsetX: CGFloat = 0
+    var offsetY: CGFloat  = 0
+    var delay: Double = 0.0
+    var color: Color = .white
+    
+    var body: some View {
+        Button(action: action) {
+            Image(systemName: icon)
+                .foregroundColor(.white)
+                .frame(width: 35, height: 35)
+                .font(.system(size: 16, weight: .bold))
+
+        }
+        .background(color)
+        .clipShape(Circle())
+        .overlay(
+            Circle().stroke(Color.white, lineWidth: 3))
+        .padding([.horizontal], 5)
+        .shadow(radius: 5)
+        .offset(x: open ? offsetX : 0, y: open ? offsetY : 0)
+        .scaleEffect(open ? 1 : 0)
+        .animation(Animation.spring(response: 0.2, dampingFraction: 0.5, blendDuration: 0).delay(delay))
     }
 }
